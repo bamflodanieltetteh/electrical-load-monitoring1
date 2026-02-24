19ab43db88c9631493441b13ca4dc4b0c78a2de9
bamflodanieltetteh70-hue <bamflodanieltetteh70@gmail.com>
2026-02-24 06:34:55 +0000

    Week 3: Improved menu structure and separated billing features into distinct cases

--- main.cpp
+++ main.cpp
@@ -36,14 +36,23 @@ class Appliance {
     double calculateEnergy() const {
         return (power * hours) / 1000.0;
     }
-    void displayAppliance() const {
+   void displayAppliance(double tariff) const {
     cout << "Appliance Name: " << name << endl;
     cout << "Power Rating: " << power << " W" << endl;
     cout << "Daily Usage: " << hours << " hours" << endl;
-    cout << "Daily Energy Consumption: " 
-         << calculateEnergy() << " kWh" << endl;
+    cout << "Daily Energy: " << calculateEnergy() << " kWh" << endl;
+    cout << "Monthly Energy: " << calculateMonthlyEnergy() << " kWh" << endl;
+    cout << "Monthly Cost: " 
+         << calculateMonthlyCost(tariff) << endl;
     cout << "--------------------------------------" << endl;
- }
+}
+ double calculateMonthlyEnergy() const {
+    return calculateEnergy() * 30;
+}
+
+double calculateMonthlyCost(double tariff) const {
+    return calculateMonthlyEnergy() * tariff;
+}
 };
         
 int main() {
@@ -52,49 +61,76 @@ int main() {
     int count = 0;
     int choice;
 
+    double tariff = 1.5;
+    
     do {
-        cout << "\n== Electrical Load Monitoring System ==\n";
+        cout << "\n=== Electrical Load Monitoring System ===\n";
         cout << "1. Register Appliance\n";
         cout << "2. View All Appliances\n";
+        cout << "3. View Total Monthly Bill\n";
+        cout << "4. System Status\n";
         cout << "0. Exit\n";
-        cout << "Enter Choice: ";
+        cout << "Enter choice: ";
         cin >> choice;
+        
         switch (choice) {
             case 1:
-            if (count < MAX) {
-                appliances[count].setAppliance();
-                count++;
-                cout << "Appliance registered successfully!\n";
-            } else {
-                cout << "Appliance list is full!\n";
-            }
-            break;
+                if (count < MAX) {
+                    appliances[count].setAppliance();
+                    count++;
+                    cout << "Appliance registered successfully!\n";
+                } else {
+                    cout << "Appliance list is full!\n";
+                }
+                break;
 
             case 2:
-            if (count == 0) {
-                cout << "No appliances registered yet.\n";
-            } else {
-                double totalEnergy = 0;
+                if (count == 0) {
+                    cout << "No appliances registered yet.\n";
+                } else {
+                    for (int i = 0; i < count; i++) {
+                        appliances[i].displayAppliance(tariff);
+                    }
+                }
+                break;
 
-             for (int i = 0; i < count; i++) {
-                  appliances[i].displayAppliance();
-              totalEnergy += appliances[i].calculateEnergy();
-         }
-             cout << "Total Daily Energy Consumption: "
-            << totalEnergy << " kWh\n";            
-        }
-            break;
+            case 3:
+                if (count == 0) {
+                    cout << "No appliances registered yet.\n";
+                } else {
+                    double totalMonthlyEnergy = 0;
+                    double totalBill = 0;
+
+                    for (int i = 0; i < count; i++) {
+                        totalMonthlyEnergy += appliances[i].calculateMonthlyEnergy();
+                        totalBill += appliances[i].calculateMonthlyCost(tariff);
+                    }
+
+                    cout << "Total Monthly Energy: "
+                         << totalMonthlyEnergy << " kWh\n";
+
+                    cout << "Estimated Monthly Electricity Bill: "
+                         << totalBill << endl;
+                }
+                break;
+
+            case 4:
+                if (count == 0) {
+                    cout << "System Status: No appliances registered.\n";
+                } else {
+                    cout << "System Status: "
+                         << count << " appliance(s) registered.\n";
+                }
+                break;
 
             case 0:
-            cout << "Exiting program...\n";
-            break;
+                cout << "Exiting program...\n";
+                break;
 
             default:
-            cout << "Invalid choice! Try again.\n";
-
+                cout << "Invalid choice! Try again.\n";
         }
-
     } while (choice != 0);
-
+    
     return 0;
 }
